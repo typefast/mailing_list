81d555a93e62986a653c6e8abbbe4db13b9a6d90
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-05 19:13:57 +0000

    Add new and create actions visitor controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,15 @@
 class VisitorsController < ApplicationController
+  
+  def new
+    @visitor = Visitor.new
+  end
+  
+  def create
+    @visitor = Visitor.new(params.require(:visitor).permit(:email))
+    if @visitor.save
+      redirect_to root_path, notice: "Signed up #{@visitor.email}"
+    else
+      render :new
+    end
+  end
 end
