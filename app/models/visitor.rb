2e2afbeed51eb8212b8312c7e1e2667f0aa5003b
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-05 19:38:10 +0000

    Add subscribe method to visitor model

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,2 +1,18 @@
 class Visitor < ActiveRecord::Base
+  
+  validates_presence_of :email
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  after_create :subscribe
+  
+  def subscribe
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+     body: {
+       email_address: self.email,
+       status: 'subscribed'
+    })
+    Rails.logger.info("subscribed #{self.email} to MailChimp") if result
+    
+  end
 end
