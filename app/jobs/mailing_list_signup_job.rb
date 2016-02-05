9b552b120da2de0089f87009a61433a18705f2ff
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-05 21:27:44 +0000

    Update mailinglist job subscribe method

--- app/jobs/mailing_list_signup_job.rb
+++ app/jobs/mailing_list_signup_job.rb
@@ -2,7 +2,22 @@ class MailingListSignupJob < ActiveJob::Base
   
   def perform(user)
     logger.info "signing up #{user.email}"
-    user.subscribe
+    subscribe(user)
+  end
+  
+  def subscribe(user)
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+     body: {
+       email_address: user.email,
+       status: 'subscribed'
+    })
+    Rails.logger.info("subscribed #{self.email} to MailChimp") if result
+    
+    rescue Gibbon::MailChimpError => e
+      Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)  
+    
   end
   
 end
