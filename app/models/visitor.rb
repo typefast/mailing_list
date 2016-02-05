16fc87e6d2d3c658b6b668ff0755bcf06559f608
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-05 21:35:21 +0000

    Update Visitor model

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,17 +5,6 @@ class Visitor < ActiveRecord::Base
   after_create :subscribe
   
   def subscribe
-    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
-    list_id = Rails.application.secrets.mailchimp_list_id
-    result = mailchimp.lists(list_id).members.create(
-     body: {
-       email_address: self.email,
-       status: 'subscribed'
-    })
-    Rails.logger.info("subscribed #{self.email} to MailChimp") if result
-    
-    rescue Gibbon::MailChimpError => e
-      Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)  
-    
+    MailingListSignupJob.perform_later(self)
   end
 end
