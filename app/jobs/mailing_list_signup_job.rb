a7212b6c87a964b2f0e543be1e7af10ffc325778
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-06 19:37:35 +0000

    Add sucker_punch.rb initializer to use old syntax

--- app/jobs/mailing_list_signup_job.rb
+++ app/jobs/mailing_list_signup_job.rb
@@ -1,23 +1,19 @@
 class MailingListSignupJob < ActiveJob::Base
-  
+
   def perform(user)
     logger.info "signing up #{user.email}"
     subscribe(user)
   end
-  
+
   def subscribe(user)
     mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
     list_id = Rails.application.secrets.mailchimp_list_id
     result = mailchimp.lists(list_id).members.create(
-     body: {
-       email_address: user.email,
-       status: 'subscribed'
+      body: {
+        email_address: user.email,
+        status: 'subscribed'
     })
-    Rails.logger.info("subscribed #{self.email} to MailChimp") if result
-    
-    rescue Gibbon::MailChimpError => e
-      Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)  
-    
+    Rails.logger.info("Subscribed #{user.email} to MailChimp") if result
   end
-  
+
 end
