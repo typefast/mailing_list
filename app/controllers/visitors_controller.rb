9e31b91fc90e6f831c665d6ad79631ccf97fb9b5
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-07 10:33:26 +0000

    modify visitor controller create method

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,13 @@
 class VisitorsController < ApplicationController
   
+  protect_from_forgery except: :create
+  
   def new
     @visitor = Visitor.new
   end
   
   def create
-    @visitor = Visitor.new(params.require(:visitor).permit(:email))
+    @visitor = Visitor.new(params.require(:visitor).permit(:email, :affinity))
     if @visitor.save
       redirect_to root_path, notice: "Signed up #{@visitor.email}"
     else
