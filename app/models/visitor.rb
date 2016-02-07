4252cffc4580ce105a748cdf0c4a91d442b3faf1
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-07 09:13:16 +0000

    Add default method to visitor model

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,6 +3,11 @@ class Visitor < ActiveRecord::Base
   validates_presence_of :email
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   after_create :subscribe
+  after_initialize :defaults
+  
+  def defaults
+    self.affinity = 'NONE' unless self.affinity == 'KITTENS'
+  end
   
   def subscribe
     MailingListSignupJob.perform_later(self)
